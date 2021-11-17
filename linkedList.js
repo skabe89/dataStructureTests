1cf9704903c2813b8c41f084d207f99722822dbe
Sean Abe <skabe89@gmail.com>
2021-11-16 20:06:03 -0500

    linkedList class

---
+++ linkedList.js
@@ -0,0 +1,30 @@
+class Node {
+    constructor(val){
+        this.val = val
+        this.next = null
+    }
+}
+
+class LinkedList{
+    constructor(){
+        this.head = null
+        this.length = 0
+    }
+
+    insert(val){
+        let node = new Node(val)
+        if(!this.head){
+            this.head = node
+            this.length++
+        }
+        else{
+            let curr = this.head
+            while(curr){
+                curr = curr.next
+            }
+            curr.next = node
+            this.length++
+        }
+    }
+    
+}
